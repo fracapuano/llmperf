535b393331ccae0cac33b2cc0302433816b3598e
fracapuano <francesco_capuano@aol.com>
2024-05-07 17:16:16 +0200

    remove: useless files

--- src/llmperf/common.py
+++ src/llmperf/common.py
@@ -1,15 +1,8 @@
 from typing import List
-from llmperf.ray_clients.litellm_client import LiteLLMClient
-from llmperf.ray_clients.openai_chat_completions_client import (
-    OpenAIChatCompletionsClient,
-)
-from llmperf.ray_clients.sagemaker_client import SageMakerClient
-from llmperf.ray_clients.vertexai_client import VertexAIClient
 from llmperf.ray_llm_client import LLMClient
+from llmperf.ray_clients.litellm_client import LiteLLMClient
 
-
-SUPPORTED_APIS = ["openai", "anthropic", "litellm"]
-
+SUPPORTED_APIS = ["litellm"]
 
 def construct_clients(llm_api: str, num_clients: int) -> List[LLMClient]:
     """Construct LLMClients that will be used to make requests to the LLM API.
@@ -22,13 +15,7 @@ def construct_clients(llm_api: str, num_clients: int) -> List[LLMClient]:
         The constructed LLMCLients
 
     """
-    if llm_api == "openai":
-        clients = [OpenAIChatCompletionsClient.remote() for _ in range(num_clients)]
-    elif llm_api == "sagemaker":
-        clients = [SageMakerClient.remote() for _ in range(num_clients)]
-    elif llm_api == "vertexai":
-        clients = [VertexAIClient.remote() for _ in range(num_clients)]
-    elif llm_api in SUPPORTED_APIS:
+    if llm_api in SUPPORTED_APIS:
         clients = [LiteLLMClient.remote() for _ in range(num_clients)]
     else:
         raise ValueError(
